094aaf62a914a630c075147bb00716f7be6f5fb1
Alok <alok48ax@gmail.com>
2024-09-08 08:00:51 +0530

    Update ListingCard.jsx

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -60,7 +60,7 @@ const ListingCard = ({
           {listingPhotoPaths?.map((photo, index) => (
             <div key={index} className="slide">
               <img
-                src={`http://localhost:3001/${photo?.replace("public", "")}`}
+                src={`https://dream-nest-server.onrender.com/${photo?.replace("public", "")}`}
                 alt={`photo ${index + 1}`}
               />
 
