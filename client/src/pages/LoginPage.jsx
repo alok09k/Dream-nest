e0ef8b1e5bb4a50238127164886412ece2b98287
Alok <alok48ax@gmail.com>
2024-09-08 08:03:48 +0530

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -18,7 +18,7 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3001/auth/login", {
+      const response = await fetch("https://dream-nest-server.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
